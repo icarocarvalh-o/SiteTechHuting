90a5dbfc6374b2ef8c6e2aece75fd41542b96a9a
Icaro Pereira <icaro.pereira@sisconsultoria.com.br>
2022-12-02 11:54:28 -0300

    Codigo JS para simular o login corporativo

---
+++ script/login.js
@@ -0,0 +1,6 @@
+var botaoLogin = document.querySelector("#botao-login");
+
+botaoLogin.addEventListener("click", function(event) {
+    event.preventDefault();
+    window.location.href = "index.html";
+})
